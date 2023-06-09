a81aba9725fcc182655d1569838f545df59287e4
Oleksandra Andrushchenko <oleksandraandrusenko6@gmail.com>
2023-06-09 22:19:34 +0300

    add contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { Section } from "./Section/Section";
+import { ContactForm } from "./Contact/Contact";
+import Notiflix from "notiflix";
+import { Container } from './App.styled'
+
+
+export default class App extends Component {
+	state = {
+		contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+		filter: '',
+	}
+
+	addContact = newContact => {
+		this.state.contacts.filter(
+			contact =>
+				contact.name.toLowerCase().trim() ===
+				newContact.name.toLowerCase().trim() ||
+				contact.number.trim() === newContact.number.trim()
+		).length
+			? Notiflix.Report.warning('warning', `${newContact.name} is already in contacts.`)
+			: this.setState(prevState => {
+				return {
+					contacts: [newContact, ...prevState.contacts],
+				};
+			});
+	};
+
+	render() {
+		return (
+		<Container>
+			<Section title="Phonebook">
+				<ContactForm onAddContact={this.addContact} />
+
+			</Section>
+		</Container>
+		);
+	}
+}
+
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 16px;
+	width: 100%;
+	padding: 16px;
+		/* margin: 0 auto; */
+	/* text-align: center; */
+	background: linear-gradient(#bef8ec, #91e5d6);;
+	/* box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0 4px 4px rgba(0, 0, 0, 0.06), 1px 4px 6px rgba(0, 0, 0, 0.16); */
+`;
